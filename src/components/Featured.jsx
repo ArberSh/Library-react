87ed5a39d77470be111acb6a7a6bbe914c95a82b
ArberSh <arbershaska2@gmail.com>
2023-09-28 22:36:55 +0200

    Added Featured and fixed css

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Book from './ui/Book'
 
 export default function Featured() {
   return (
-    <div>
-        Featured
+    <section id='features'>
+        <div className='container'>
+            <div className='row'>
+                <h2 className='section__title'>
+                    Featured <span className='purple'>Books</span>
+                </h2>
+                <div className='books'>
+                    <Book/>
+                    <Book/>
+                    <Book/>
+                    <Book/>
+                </div>
+            </div>
         </div>
+    </section>
   )
 }
