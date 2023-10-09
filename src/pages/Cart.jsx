032ca4aa374db09a8ac1316772170498d9e14a29
ArberSh <arbershaska2@gmail.com>
2023-10-09 23:14:47 +0200

    Added some codes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,8 +18,39 @@ export default function Cart() {
                         <div className='cart__item'>
                             <div className='cart__book'>
                                 <img src='https://covers.openlibrary.org/b/id/8091016-L.jpg' className='cart__book--img' alt=''></img>
+                                <div className='cart__book--info'>
+                                    <span className='cart__book--title'>
+                                        Crack the coding interview
+                                    </span>
+                                    <span className='cart__book--price'>
+                                        $10,000
+                                    </span>
+                                    <button className='cart__book--remove'>
+                                        Remove
+                                    </button>
+                                </div>
+                            </div>
+                            <div className='cart__quantity'>
+                                <input type='number' min={0} max={99} className='cart__input'/>
+                            </div>
+                            <div className='cart__total'>
+                                $10.00
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className='total'>
+                    <div className='total__item total__sub-total'>
+                        <span>Subtotal</span>
+                        <span>$9.00</span>
+                    </div>
+                    <div className='total__item total__tax'>
+                        <span>Tax</span>
+                        <span>$1.00</span>
+                    </div>
+                    <div className='total__item total__price'>
+                        <span>Total</span>
+                        <span>$10.00</span>
                     </div>
                 </div>
             </div>
